a285087547f0f9b7881eb6109907064d1d7bb871
주식회사토닥 (이호승) <todoc.tech@gmail.com>
2025-09-17 07:18:27 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  "projectId": "studio-4065495492-bfd71",
+  "appId": "1:1003104102687:web:973807fd304b15c446282a",
+  "storageBucket": "studio-4065495492-bfd71.firebasestorage.app",
+  "apiKey": "AIzaSyC50olFk62pBKZfxVpq6ynd-o4eJudxuJc",
+  "authDomain": "studio-4065495492-bfd71.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1003104102687"
 };
 
 // Initialize Firebase
