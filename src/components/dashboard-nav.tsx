3da12c0d554e5eb2e26f00764c7871832d6983d3
주식회사토닥 (이호승) <todoc.tech@gmail.com>
2025-09-17 05:38:10 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/components/dashboard-nav.tsx
+++ src/components/dashboard-nav.tsx
@@ -18,25 +18,22 @@ export function DashboardNav() {
     <SidebarMenu>
       {navItems.map((item) => (
         <SidebarMenuItem key={item.href}>
-          <Link href={item.href} passHref legacyBehavior>
-            <SidebarMenuButton
-              asChild
-              className={cn(
-                'group-data-[collapsible=icon]:justify-center',
-                'group-data-[collapsible=icon]:h-10 group-data-[collapsible=icon]:w-10'
-              )}
-              isActive={pathname.startsWith(item.href)}
-              tooltip={{
-                children: item.label,
-                className: 'group-data-[collapsible=icon]:hidden',
-              }}
-            >
-              <a >
-                <item.icon className="size-5 shrink-0" />
-                <span className="min-w-max">{item.label}</span>
-              </a>
-            </SidebarMenuButton>
-          </Link>
+          <SidebarMenuButton
+            as={Link}
+            href={item.href}
+            className={cn(
+              'group-data-[collapsible=icon]:justify-center',
+              'group-data-[collapsible=icon]:h-10 group-data-[collapsible=icon]:w-10'
+            )}
+            isActive={pathname.startsWith(item.href)}
+            tooltip={{
+              children: item.label,
+              className: 'group-data-[collapsible=icon]:hidden',
+            }}
+          >
+            <item.icon className="size-5 shrink-0" />
+            <span className="min-w-max">{item.label}</span>
+          </SidebarMenuButton>
         </SidebarMenuItem>
       ))}
     </SidebarMenu>
