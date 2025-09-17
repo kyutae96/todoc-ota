ae6a5ff755c47cf64f9407dafbffe37eb9e93804
주식회사토닥 (이호승) <todoc.tech@gmail.com>
2025-09-17 08:40:15 +0000

    /devices/TD_F_SOUND_OTE12_5181/otaSessions/13n92clKa7xPOdbeAysG
    this is

--- src/app/dashboard/devices/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-import { SessionDetails } from "@/components/session-details";
-
-export default function DeviceDetailsPage({ params }: { params: { id: string } }) {
-  // This page shows session details, but is under device route.
-  // This should be changed to reflect session details.
-  // For now, we pass the session ID to the SessionDetails component.
-  return <SessionDetails sessionId={params.id} />;
-}
